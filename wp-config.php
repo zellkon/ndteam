c8d5bb33e87e7929591a73c79850926653dc737d
NDTeam <zellkon@live.com>
2019-02-16 16:52:06 +0700

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'database');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'zk17041199');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
